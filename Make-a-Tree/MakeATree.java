0b384f6d9f493f63a7e29ca474a8d4c84c5bd9da
MarkMoseley <mmoseley91@gmail.com>
2016-02-05 18:37:18 -0500

    MakeATree.java
    This program creates an isosceles triangle or "tree" based on a height given by the user. The tree is composed of '*' characters.

---
+++ Make-a-Tree/MakeATree.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+/** Description:
+     
+     This program creates an isosceles triangle or "tree" based on a height given
+     by the user. The tree is composed of '*' characters.
+     
+     */
+
+public class MakeATree{
+
+   public static void main (String [] args){
+      
+      //Sets up the Scanner Object
+      Scanner kbd = new Scanner(System.in);
+   
+      //Give out instructions to the user
+      System.out.println("This program will make a tree of \"*'s\" based on a given height.");
+      
+      //Moves the cursor down a line
+      NextLine();
+      
+      //Ask for the height of the tree
+      System.out.print("Please enter an integer value greater than one for the height of the tree: ");
+      
+      //Save the height of the tree as a double
+      double height = kbd.nextDouble();
+        
+      NextLine();
+         
+         //Test to make sure only a valid integer for height is entered
+         while(height <= 1){
+            System.out.print("Please enter an integer value GREATER than one for the height of the tree: ");
+            height=kbd.nextDouble();
+            NextLine();
+         }
+         
+         //Test to make sure only an integer has been entered
+         while(height%1 != 0){
+            System.out.print("Please enter only an INTEGER value greater than one for the height of the tree: ");
+            height=kbd.nextDouble();
+            NextLine();
+         }
+
+      //Draw the tree
+      
+      //controls which line of the tree is being worked upon
+      for( int i = 1; i <= height; i++){
+         
+         //controls the amount of spaces
+         for( int j = 1; j<= height -i; j++)
+            System.out.print(" ");
+         
+         //controls the amount of characters
+         for( int j =1; j<=i ; j++){
+            System.out.print("* ");
+         }
+            
+      //Moves the cursor down
+      NextLine();
+      }
+   
+   //End Main
+   }
+   
+   //Creates a method to move the cursor down a line
+   public static void NextLine(){
+      System.out.println();
+   }
+   
+}
